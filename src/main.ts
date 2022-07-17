bd2f39b1e6bf002007c4f39f1a05040c475b2f1f
Andrey Peshko <coursestud@mail.ru>
2022-07-17 11:16:41 +0300

    refactor: add env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
