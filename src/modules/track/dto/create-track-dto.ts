d3a9a6665580056dcfd7b8156690370a68b418a7
Andrey Peshko <coursestud@mail.ru>
2022-07-30 20:37:34 +0300

    feat: implement track module

--- src/modules/track/dto/create-track-dto.ts
+++ src/modules/track/dto/create-track-dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ARTIST_MESSAGE } from 'src/utils/constant';
+
 export class CreateTrackDto {
+  @IsString({ message: ARTIST_MESSAGE.no_fields_required })
+  @IsNotEmpty()
   readonly name: string;
+
   readonly artistId: string | null;
+
   readonly albumId: string | null;
+
+  @IsNumber()
+  @IsNotEmpty({ message: ARTIST_MESSAGE.no_fields_required })
   readonly duration: number;
 }
